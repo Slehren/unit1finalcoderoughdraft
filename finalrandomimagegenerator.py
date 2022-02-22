3c7f4bc8358b19cda8eed68df84e58ec44916775
Slehren <99208461+Slehren@users.noreply.github.com>
2022-02-22 14:39:55 -0500

    Update finalrandomimagegenerator.py
    @rors can you give me feedback on this firstdraft

---
+++ finalrandomimagegenerator.py
@@ -0,0 +1,30 @@
+
+import fileinput "images"
+from os import listdir, path
+import random
+from PIL import Image
+
+files = listdir("images")
+files.remove(".DS_Store")
+
+random_file = random.choice(files)
+
+img = Image.open( path.join("images",random_file) )
+
+
+# let's make a 100x100 white image
+
+width = 100
+height = 100
+
+or x in range(width):
+    for y in range(height):
+        pixel = new_img.getpixel((x,y))
+        if pixel[1] > 200
+
+img = Image.new("RGB", 100x100,100x100) )
+
+
+img.putdata(data)
+
+img.save(sys.argv[1])
